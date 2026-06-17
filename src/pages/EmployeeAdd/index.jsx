98cf375db1117cedcda56dc9c5b51fc3d5c540e6
arunima9e-bit <arunima.9e@gmail.com>
2026-06-17 12:03:21 +0530

    form

--- src/pages/EmployeeAdd/index.jsx
+++ src/pages/EmployeeAdd/index.jsx
@@ -1,3 +1,8 @@
-export default function EmployeeAdd() {
-  return <div>Employee Add Page Placeholder</div>;
+export default function EmployeeAddPage() {
+  return (
+    <div>
+      <h1>Employee Add Page</h1>
+      <p>Member 4 Branch Working!</p>
+    </div>
+  );
 }
--- src/pages/EmployeeAddPage.jsx
+++
@@ -1,8 +0,0 @@
-export default function EmployeeAddPage() {
-  return (
-    <div>
-      <h1>Employee Add Page</h1>
-      <p>Member 4 Branch Working!</p>
-    </div>
-  );
-}
